d02064439e88f65761ef420622f92c5d975e728c
Micha Schmid <micha_schmid@bluewin.ch>
2019-11-30 19:01:07 +0100

    Added simplymove

---
+++ precache-manifest.533383d58d841a13562fe701f199999b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b391e638de6338f8374c",
+    "url": "./css/app.db85be9d.css"
+  },
+  {
+    "revision": "d3358e09422d7abb0671",
+    "url": "./css/chunk-vendors.79ba8df5.css"
+  },
+  {
+    "revision": "2b67051b05c5e959da11",
+    "url": "./css/home.77fddadf.css"
+  },
+  {
+    "revision": "6a44704966747c6a9d0a",
+    "url": "./css/home~move.291646f7.css"
+  },
+  {
+    "revision": "1e6bc063466fbfcc62cb",
+    "url": "./css/move.3a2e4d60.css"
+  },
+  {
+    "revision": "b662eab6f9b6479ff3f027c6fd35b568",
+    "url": "./img/Sitemap.b662eab6.svg"
+  },
+  {
+    "revision": "dd31498cb22c94233b4d6f1c975b3679",
+    "url": "./img/gray_wooden_house_unsplash.dd31498c.jpg"
+  },
+  {
+    "revision": "59e5ecc1542d6ae0514cfc1716c7b975",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b391e638de6338f8374c",
+    "url": "./js/app.3017e220.js"
+  },
+  {
+    "revision": "d3358e09422d7abb0671",
+    "url": "./js/chunk-vendors.5bbd3bde.js"
+  },
+  {
+    "revision": "2b67051b05c5e959da11",
+    "url": "./js/home.cbda6b25.js"
+  },
+  {
+    "revision": "6a44704966747c6a9d0a",
+    "url": "./js/home~move.74d57398.js"
+  },
+  {
+    "revision": "1e6bc063466fbfcc62cb",
+    "url": "./js/move.508b6b46.js"
+  },
+  {
+    "revision": "8f30764db400269825e90ca0aefb2e4a",
+    "url": "./manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "./robots.txt"
+  }
+]);
